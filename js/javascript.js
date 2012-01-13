046c7fa102f2a44b7a97faa44469ded945854fb5
vank0026 <roger.van.koughnett@gmail.com>
2012-01-13 11:35:27 -0500

    more neat things

--- js/javascript.js
+++ js/javascript.js
@@ -6,3 +6,11 @@
 the single slash and star is the
 multi line comment thing, just like AS3
 */
+
+alert('This is an alert.');  // makes a popup window
+
+var name = prompt("What's your name?");
+
+console.log(name); // works line the trace() thing in AS3
+
+document.write("Hello, " + name);
