527105a52103fd4af5ec56e839b721b6e2321a29
vank0026 <roger.van.koughnett@gmail.com>
2012-01-13 12:15:39 -0500

    some loops

--- js/javascript.js
+++ js/javascript.js
@@ -9,8 +9,33 @@ multi line comment thing, just like AS3
 
 alert('This is an alert.');  // makes a popup window
 
-var name = prompt("What's your name?");
+var name;
 
-console.log(name); // works line the trace() thing in AS3
+while (!name){	// the exclamation point means not
+name = prompt("What's your name?");
+}
 
-document.write("Hello, " + name);
+console.log(name); // works line the trace() thing in AS3 - find through firebug console
+
+document.write("Hello, " + name); // will make Hello, Roger (or other name) appear onscreen
+
+for (var i = 0; i < 5; i++){
+	document.write("<p>" + i);  // for js, html is just a string - to get the html to work, put in "
+	document.write("<h1>" + i + "</h1>");
+}
+
+//		FUNCTIONS 
+
+// declarations
+
+function isNameCool () {
+	var name = prompt('What is yuohr name?');
+	
+	if (name == "Roger") {
+		document.write("That is an awesome name!");
+	}else{
+		document.write("You have chosen poorly.");
+	}
+}
+
+isNameCool();
